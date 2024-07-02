922fdd3bad2791b43c09398f8d950f00b3706ee0
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-07-02 11:12:41 +0200

    Web responsive fixed

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -4,15 +4,15 @@ import About from "../../Components/About/About";
 
 function Home() {
   return (
-    <div className="grid gap-2 p-10 md:grid-cols-2">
-      <div className="flex  justify-center flex-col gap-5">
+    <div className="grid gap-2 items-center p-10 md:grid-cols-2">
+      <div className="flex flex-col gap-5">
         <h4>HI THERE 👋 I'M</h4>
         <h1 className="text-6xl font-bold">Kgosiyagae Motabogi</h1>
         <h4 className="text-blue-500">DEVELOPER + UI/UX DESIGNER</h4>
         <p>I'm a professional App developer with UI/UX Designer skills based in kimberley</p>
         <button className="w-[150px] bg-blue-500 p-3 rounded-3xl hover:bg-blue-600">Hire Me</button>
       </div>
-      <div className="flex items-center justify-center">
+      <div className="flex">
         <img src="/sticker-laptop.webp" alt="kgosi's picture" />
       </div>
     </div>
